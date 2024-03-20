34eff0f684bf2d53276b4acc865c267e72bea64b
kenny <kennycoder971@gmail.com>
2024-03-20 13:58:17 +0100

    Writing css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url =
   "http://api.weatherapi.com/v1/current.json?key=acabd4755e0b4a88a3c100431231408&q=Bordeaux&aqi=no";
 
-const weatherCard = document.querySelector(".weather-card");
+const body = document.querySelector("body");
 
 async function getWeather() {
   const response = await fetch(url);
@@ -9,32 +9,36 @@ async function getWeather() {
   return data;
 }
 
-getWeather().then((data) => {
-  const html = `<div class="weather-card-top">Weather App</div>
-  <div class="weather-card-mid">
-      <div class="weather-icon"></div>
-      <div class="weather-temperature">${data.current.temp_c}°C</div>
-      <div class="weather-condition">
-          ${data.current.condition.text}
-      </div>
-      <div>City</div>
-  </div>
-  <div class="weather-card-bottom">
-      <div class="weather-bottom-left">
-          <div class="weather-bottom-left-icon">Icon</div>
-          <div class="weather-bottom-left-feels-like">
-              <p class="weather-bottom-left-feels-like-num">${data.current.feelslike_c}°C</p>
-              <p class="weather-bottom-left-feels-like">Feels like</p>
-          </div>
-      </div>
-      <div class="weather-bottom-right">
-          <div class="weather-bottom-right-icon">Icon</div>
-          <div class="weather-bottom-right-humidity">
-              <p class="weather-bottom-right-humidity-num">${data.current.humidity}%</p>
-              <p class="weather-bottom-right-humidity">Humidity</p>
-          </div>
-      </div>
-  </div>`;
+https: getWeather().then((data) => {
+  console.log(data);
+  const html = ` <article class="weather-card">
+  <h1 class="weather-card-top">Weather App</h1>
+    <div class="weather-card-mid">
+        <div class="weather-card-mid-icon"><img src="https:${data.current.condition.icon}"/> </div>
+        <p class="weather-card-mid-temperature">${data.current.temp_c}°C</p>
+        <p class="weather-card-mid-condition">
+            ${data.current.condition.text}
+        </p>
+        <p class="weather-card-mid-city"><i class="fa-solid fa-location-dot"></i> ${data.location.name}</p>
+    </div>
+    <div class="weather-card-bottom">
+        <div class="weather-card-bottom-left">
+            <div class="weather-card-bottom-left-icon"><i class="fa-solid fa-temperature-three-quarters"></i></div>
+            <div class="weather-card-bottom-left-feels-like">
+                <p class="weather-card-bottom-left-feels-like-num">${data.current.feelslike_c}°C</p>
+                <p class="weather-card-bottom-left-feels-like">Feels like</p>
+            </div>
+        </div>
+        <div class="weather-card-bottom-right">
+            <div class="weather-card-bottom-icon"><i class="fa-solid fa-droplet"></i></div>
+            <div class="weather-card-bottom-right-humidity">
+                <p class="weather-card-bottom-right-humidity-num">${data.current.humidity}%</p>
+                <p class="weather-card-bottom-right-humidity">Humidity</p>
+            </div>
+        </div>
+    </div>
+  </article>
+  `;
 
-  weatherCard.innerHTML = html;
+  body.innerHTML = html;
 });
